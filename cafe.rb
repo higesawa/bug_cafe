b725652dcfd8d43de937170fe9922dd1d5a30a6d
higesawa <C.yaoce2.5@gmail.com>
2024-06-11 21:30:16 +0900

    変数名が何を指すか分かるように修正

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_num = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food_num = take_order(FOODS)
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drink_num][:price].to_i + FOODS[food_num][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
